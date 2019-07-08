3d40367dfd43d471410c480f3687bd1c3a302a5e
cmatri <cperkin4@gmu.edu>
2019-07-07 20:33:16 -0400

    Complete notifications, bug fixes

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,39 @@ use eventual::Timer;
 use notify_rust::Notification;
 use std::sync::mpsc::Sender;
 use std::process::Command;
+use std::time::Duration;
 use std::thread;
 
+static PHONE_CONNECTED: &'static str = "smartphonetrusted";
+static PHONE_DISCONNECTED: &'static str = "smartphonedisconnected";
+
+fn sleep(dur: u64) {
+    thread::sleep(Duration::from_millis(dur));
+}
+
+fn show_notification(msg: &'static str, icon: &'static str, delay: u64) {
+    thread::spawn(move || {
+        sleep(delay);
+        Notification::new()
+            .summary("Tether Manager")
+            .body(msg)
+            .icon(icon)
+            .show().unwrap();
+    });         
+}
+
 fn start_openvpn() {
-    Command::new("gksu")
+    Command::new("sudo")
         .arg("/home/meet/.config/android_tether/start_ovpn.sh")
         .spawn()
         .expect("Failed to start OpenVPN");
 }
 
 fn kill_openvpn() {
-    let cmd = Command::new("gksu").arg("pkill openvpn").spawn().expect("Failed to kill OpenVPN");
+    Command::new("sudo")
+        .arg("/home/meet/.config/android_tether/end_ovpn.sh")
+        .spawn()
+        .expect("Failed to start OpenVPN");
 }
 
 fn start_timer(context: &libusb::Context, phone_vendor_id: u16, phone_product_id: u16, phone_product_id_hid: u16) {
@@ -34,20 +56,15 @@ fn start_timer(context: &libusb::Context, phone_vendor_id: u16, phone_product_id
                 } else {
                     if cur_hid != device_desc.product_id() {
                         kill_openvpn();
+                        sleep(6000);
                         start_openvpn();
                         cur_hid = device_desc.product_id();
                     }
                 }
 
-                println!("Found phone!");
-                if openvpn_killed{
+                if openvpn_killed {
                     start_openvpn();
-                    println!("Started OpenVPN.");
-                    Notification::new()
-                        .summary("Started OpenVPN")
-                        .body("I just started OpenVPN for you.")
-                        .icon("firefox")
-                        .show().unwrap();
+                    show_notification("Initiated tether", PHONE_CONNECTED, 0);
                     openvpn_killed = false;
                 }
                 
@@ -57,10 +74,10 @@ fn start_timer(context: &libusb::Context, phone_vendor_id: u16, phone_product_id
                 phone_unplugged = true;
             }
         }
-        println!("Phone unplugged: {}", phone_unplugged);
 
         if phone_unplugged && !openvpn_killed {
             kill_openvpn();
+            show_notification("Severed tether", PHONE_DISCONNECTED, 0);
             openvpn_killed = true;
         }
     }
